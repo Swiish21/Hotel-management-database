ab65360102a874ce4bb957eedc953832b03b97a9
Swiish21 <154547110+Swiish21@users.noreply.github.com>
2024-07-03 10:41:49 +0300

    Add files via upload

---
+++ Hotel management database.sql
@@ -0,0 +1,16 @@
+-- Create database amazon;
+-- use amazon;
+
+-- create table rooms1(
+-- room_id varchar(50),
+-- room_type varchar(50),
+-- price int,
+-- is_available boolean,
+-- is_clean boolean);
+
+-- select*from rooms1;
+-- alter TABLE rooms1 add column capacity int;
+
+-- insert into rooms1(
+-- room_id, room_type, price, is_available, is_clean)
+--  value (1,'single room', 10000, true, true)
